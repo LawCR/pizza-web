2f590f171271ba7e88be6dfc9010f12554a9f892
LawCR <alvacalderon99@gmail.com>
2021-12-12 14:54:44 -0500

    Se agrego un nuevo componente a la rama2

---
+++ src/components/Prueba/nuevaRama2.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const nuevaRama2 = () => {
+    return (
+        <div>
+            <h1>Creación de nueva rama 2</h1>
+        </div>
+    )
+}
+
+export default nuevaRama2
