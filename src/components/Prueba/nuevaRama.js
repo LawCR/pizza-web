e46f73a30204947cd8a8f6cc457107451f387a58
LawCR <alvacalderon99@gmail.com>
2021-12-12 14:46:04 -0500

    Se añadio un componente a la nuevaRama

---
+++ src/components/Prueba/nuevaRama.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const nuevaRama = () => {
+    return (
+        <div>
+            <h1>Componente nueva rama</h1>
+        </div>
+    )
+}
+
+export default nuevaRama
